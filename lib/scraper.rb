7037925fdad0ad783ed239ae085537d14899258e
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-02 15:12:57 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,8 @@ require_relative './course.rb'
 class Scraper
 
   def get_page
+    html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    doc = Nokogiri::HTML(html)
   end
 
   def get_courses
